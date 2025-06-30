1f205b86a6033095ec3313c78af03519955cc1a6
kurabachew Mengistu <kmengistu1216@gmail.com>
2025-06-30 12:16:26 -0500

    Update axios baseURL to live backend URL

--- src/Axios.jsx
+++ src/Axios.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "https://api.evanforum.com/api",
-  baseURL: "http://localhost:5500/api",
+  // baseURL: "http://localhost:5500/api",
+  baseURL: "https://evangadi-forum-backend-3-4jbv.onrender.com/api",
 });
 
 export default axiosInstance;
